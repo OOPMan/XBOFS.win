35136c603b7565f910bfedb966c22800d407845a
Adam Jorgensen <adam.jorgensen.za@gmail.com>
2019-04-28 21:03:27 +0200

    Began implementning code to talk to WinUSB device

--- RazorAtroxWinUSBVigEmFeeder/main.cpp
+++ RazorAtroxWinUSBVigEmFeeder/main.cpp
@@ -4,21 +4,34 @@
 #include <curses.h>
 #include <ViGEm/Client.h>
 
+UCHAR RAZER_ATROX_INIT[] = {
+    0x05, 0x20, 0x08, 0x01, 0x05
+};
+
+struct RAZER_ATROX_DATA_PACKET
+{
+    UCHAR data[30];
+    ULONG transferred;
+};
+
 enum STATUS {
-    DISCONNECTED,
+    DISCONNECTED,    
     INIT,
     CONNECTED
 };
 
 char* statusToString(STATUS status) {
     switch (status) {
-    case DISCONNECTED: return "DISCONNECTED";
+    case DISCONNECTED: return "DISCONNECTED";    
     case INIT: return "INIT";
     case CONNECTED: return "CONNECTED";
     }
     return "UNKNOWN";
 }
 
+/*
+Render the PDCurses UI
+*/
 void renderUI(STATUS winUsbStatus, STATUS vigEmStatus) {
     erase();
     mvprintw(0, 0, "Razer Atrox WinUSB VigEm Feeder");
@@ -31,11 +44,41 @@ void renderUI(STATUS winUsbStatus, STATUS vigEmStatus) {
     refresh();
 }
 
+/*
+Sent the INIT packet to the Razor Atrox
+*/
+bool initRazorAtrox(DEVICE_DATA &winUsbDeviceData) {
+    if (winUsbDeviceData.WinusbHandle == INVALID_HANDLE_VALUE) return false;   
+    ULONG cbSent = 0;    
+    return WinUsb_WritePipe(winUsbDeviceData.WinusbHandle, 0x01, RAZER_ATROX_INIT, 5, &cbSent, 0);
+}
+
+/*
+Initialize the WinUSB device
+*/
+bool initDevice(DEVICE_DATA &winUsbDeviceData) {
+    BOOL noWinUsbDevice;
+    BOOL winUsbGetDescriptorResult;
+    USB_DEVICE_DESCRIPTOR winUsbDeviceDescriptor;
+    ULONG bytesReceived;    
+    HRESULT openWinUsbDeviceResult = OpenDevice(&winUsbDeviceData, &noWinUsbDevice);
+    if (FAILED(openWinUsbDeviceResult)) return false;     
+    winUsbGetDescriptorResult = WinUsb_GetDescriptor(
+        winUsbDeviceData.WinusbHandle, USB_DEVICE_DESCRIPTOR_TYPE, 0, 0, (PBYTE)&winUsbDeviceDescriptor, sizeof(winUsbDeviceDescriptor), &bytesReceived
+    );
+    if (winUsbGetDescriptorResult == FALSE || bytesReceived != sizeof(winUsbDeviceDescriptor)) {
+        CloseDevice(&winUsbDeviceData);
+        return false;
+    }
+    return true;
+}
+
 LONG __cdecl _tmain(LONG Argc, LPTSTR *Argv) {
     // Variables
     WINDOW *window;
     STATUS winUsbStatus = DISCONNECTED;
     STATUS vigEmStatus = DISCONNECTED;
+    DEVICE_DATA winUsbDeviceData{};
 
     // Init
     window = initscr();
@@ -44,6 +87,11 @@ LONG __cdecl _tmain(LONG Argc, LPTSTR *Argv) {
 
     // Main loop
     while (true) {
+        switch (winUsbStatus) {
+        case DISCONNECTED: winUsbStatus = initDevice(winUsbDeviceData) ? INIT : DISCONNECTED; break;
+        case INIT: winUsbStatus = initRazorAtrox(winUsbDeviceData) ? CONNECTED : DISCONNECTED; break;
+        case CONNECTED: break;
+        }        
         renderUI(winUsbStatus, vigEmStatus);
         auto key = getch();
         if (key == 'Q' || key == 'q') break;
