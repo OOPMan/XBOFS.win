af9235c5a07a017be7dab157c91a66a005b9b942
Adam Jorgensen <adam.jorgensen.za@gmail.com>
2019-07-12 23:36:23 +0200

    #1: Added threading code and devicePath member to WinUsbDevice

--- RazorAtroxWinUSBVigEmFeeder/WinUsbDevice.cpp
+++ RazorAtroxWinUSBVigEmFeeder/WinUsbDevice.cpp
@@ -2,11 +2,31 @@
 
 
 
-WinUsbDevice::WinUsbDevice()
+WinUsbDevice::WinUsbDevice(TCHAR* devicePath)
 {
+    this->devicePath = devicePath;
 }
 
 
 WinUsbDevice::~WinUsbDevice()
 {
+}
+
+DWORD WINAPI WinUsbDevice::staticRunEventLoop(void * winUsbDeviceInstance) {
+    WinUsbDevice* winUsbDevice = (WinUsbDevice*)winUsbDeviceInstance;
+    return winUsbDevice->runEventLoop();
+}
+
+DWORD WinUsbDevice::runEventLoop(void) {
+    return 0;
+}
+
+DWORD WinUsbDevice::getThreadId() {
+    return this->threadId;
+}
+
+/*
+*/
+HANDLE WinUsbDevice::runEventLoopInThread() {
+    return CreateThread(NULL, 0, staticRunEventLoop, (void*)this, 0, &this->threadId);
 }
--- RazorAtroxWinUSBVigEmFeeder/WinUsbDevice.h
+++ RazorAtroxWinUSBVigEmFeeder/WinUsbDevice.h
@@ -1,8 +1,27 @@
 #pragma once
+#include "pch.h"
+#include "easylogging++.h"
+
+#include <unordered_map>
+#include <string>
+#include <set>
+
+/*
+*/
 class WinUsbDevice
 {
 public:
-    WinUsbDevice();
+    WinUsbDevice(TCHAR* devicePath);
     ~WinUsbDevice();
+
+    static DWORD WINAPI staticRunEventLoop(void* Param);
+    DWORD getThreadId();
+    DWORD runEventLoop(void);
+    HANDLE runEventLoopInThread();
+protected:
+    TCHAR* devicePath;
+    DWORD threadId;
+    el::Logger* logger = el::Loggers::getLogger("WinUsbDevice");
+private:
 };
 
