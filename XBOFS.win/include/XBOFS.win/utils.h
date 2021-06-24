57dc7cf2af1e91ef828c6551f02824ce4b09431c
Adam Jorgensen <adam.jorgensen.za@gmail.com>
2021-06-24 09:35:24 -0400

    #3: Added enums for XBO Arcade Stick buttons and XB360 Controller buttons

--- XBOFS.win/include/XBOFS.win/utils.h
+++ XBOFS.win/include/XBOFS.win/utils.h
@@ -2,6 +2,43 @@
 #include <XBOFS.win/pch.h>
 
 namespace XBOFSWin {
+    enum class XBO_ARCADE_STICK_BUTTONS {
+        STICK_UP,
+        STICK_DOWN,
+        STICK_LEFT,
+        STICK_RIGHT,
+        X,
+        Y,
+        A,
+        B,
+        RB,
+        RT,
+        LB,
+        LT,
+        VIEW,
+        START,
+        GUIDE
+    };
+
+    enum class XB360_CONTROL_BUTTONS {
+        DPAD_UP,
+        DPAD_DOWN,
+        DPAD_LEFT,
+        DPAD_RIGHT,
+        X,
+        Y,
+        A,
+        B,
+        RB,
+        RT,
+        LB, 
+        LT,
+        VIEW,
+        START,
+        GUIDE,
+        THUMB_LEFT,
+        THUMB_RIGHT
+    };
 
     std::string utf8_encode(const std::wstring &wstr);
     std::wstring utf8_decode(const std::string &str);    
