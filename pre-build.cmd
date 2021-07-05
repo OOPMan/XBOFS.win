1ec532216ac02037205c9106396d5fd917a5e0c7
Adam Jorgensen <adam.jorgensen.za@gmail.com>
2021-07-05 12:01:57 -0400

    #5: Added libwdi to XBOFS.win.sln and added patch to allow it to build from within the XBOFS.win solution context

--- pre-build.cmd
+++ pre-build.cmd
@@ -4,5 +4,9 @@ git submodule init
 git submodule update
 ECHO Patching VigEmClient
 CD VigEmClient
-git apply ..\VigEmClient.patch 2>nul & exit 0
+git apply ..\VigEmClient.patch 2>nul
+CD ..
+ECHO Patching libwdi
+CD libwdi
+git apply ..\libwdi.patch 2>nul & exit 0
 CD ..
