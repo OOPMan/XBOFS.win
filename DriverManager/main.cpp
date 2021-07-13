750454c30854906e0a97978ce2cde61a8167b0ae
Adam Jorgensen <adam.jorgensen.za@gmail.com>
2021-07-12 22:56:52 -0400

    #21: Implemented single instance limiting feature

--- DriverManager/main.cpp
+++ DriverManager/main.cpp
@@ -1,10 +1,48 @@
 #include "DriverManager.h"
 #include <QtWidgets/QApplication>
 
+/*
+* Single instance limiting function from https://www.qtcentre.org/threads/14079-How-to-detect-that-first-instance-of-application-is-already-running
+*/
+class SingleInstanceLimiter
+{
+protected:
+    DWORD  m_dwLastError;
+    HANDLE m_hMutex;
+
+public:
+    SingleInstanceLimiter(TCHAR* strMutexName)
+    {
+        //Make sure that you use a name that is unique for this application otherwise
+        //two apps may think they are the same if they are using same name for
+        //3rd parm to CreateMutex
+        m_hMutex = CreateMutex(NULL, FALSE, strMutexName); //do early
+        m_dwLastError = GetLastError(); //save for use later...
+    }
+
+    ~SingleInstanceLimiter()
+    {
+        if (m_hMutex)  //Do not forget to close handles.
+        {
+            CloseHandle(m_hMutex); //Do as late as possible.
+            m_hMutex = NULL; //Good habit to be in.
+        }
+    }
+
+    BOOL isAnotherInstanceRunning()
+    {
+        return (ERROR_ALREADY_EXISTS == m_dwLastError);
+    }
+};
+
 int main(int argc, char *argv[])
 {
     QApplication application(argc, argv);
-    DriverManager mainWindow;
-    mainWindow.show();
-    return application.exec();
+    SingleInstanceLimiter limiter(TEXT("Global\\{58f8ed20-bc18-4523-9ec2-cd6e06f56767}"));
+    if (!limiter.isAnotherInstanceRunning()) {
+        DriverManager mainWindow;
+        mainWindow.show();
+        return application.exec();
+    }
+    return -1;
 }
--- XBOFS.win.qt/main.cpp
+++ XBOFS.win.qt/main.cpp
@@ -1,14 +1,50 @@
 #include <string>
 #include <qapplication.h>
+#include <qmessagebox.h>
 #include <qmetatype.h>
 #include <spdlog/sinks/rotating_file_sink.h>
 
 #include "XBOFSWinQTGUI.h"
 
+/*
+* Single instance limiting function from https://www.qtcentre.org/threads/14079-How-to-detect-that-first-instance-of-application-is-already-running
+*/
+class SingleInstanceLimiter
+{
+protected:
+    DWORD  m_dwLastError;
+    HANDLE m_hMutex;
+
+public:
+    SingleInstanceLimiter(TCHAR* strMutexName)
+    {
+        //Make sure that you use a name that is unique for this application otherwise
+        //two apps may think they are the same if they are using same name for
+        //3rd parm to CreateMutex
+        m_hMutex = CreateMutex(NULL, FALSE, strMutexName); //do early
+        m_dwLastError = GetLastError(); //save for use later...
+    }
+
+    ~SingleInstanceLimiter()
+    {
+        if (m_hMutex)  //Do not forget to close handles.
+        {
+            CloseHandle(m_hMutex); //Do as late as possible.
+            m_hMutex = NULL; //Good habit to be in.
+        }
+    }
+
+    BOOL isAnotherInstanceRunning()
+    {
+        return (ERROR_ALREADY_EXISTS == m_dwLastError);
+    }
+};
+
 int main(int argc, char *argv[])
 {
     QApplication application(argc, argv);
-    qRegisterMetaType<std::wstring>();    
+    SingleInstanceLimiter limiter(TEXT("Global\\{74cbcbe6-4202-4a85-9362-ae371291abd6}"));
+    qRegisterMetaType<std::wstring>();
     // Configure logging
     spdlog::set_pattern("[%Y-%m-%d %H:%M:%S.%e] [%t] [%n] [%l] %v");
     spdlog::flush_every(std::chrono::seconds(1));
@@ -17,8 +53,14 @@ int main(int argc, char *argv[])
     auto logger = XBOFSWin::get_logger("XBOFS.Win", sinks);
     logger->info("Logging initialised");
     // Display main window
-    XBOFSWinQTGUI mainWindow(XBOFSWin::get_logger("XBOFSWinQTGUI", logger->sinks()));    
-    auto result = application.exec();
+    int result = -1;
+    if (!limiter.isAnotherInstanceRunning()) {
+        XBOFSWinQTGUI mainWindow(XBOFSWin::get_logger("XBOFSWinQTGUI", logger->sinks()));
+        result = application.exec();
+    }
+    else {
+        logger->error("Another instance of XBOFS.win is already running");
+    }
     logger->info("Application exit");
     return result;
 }
